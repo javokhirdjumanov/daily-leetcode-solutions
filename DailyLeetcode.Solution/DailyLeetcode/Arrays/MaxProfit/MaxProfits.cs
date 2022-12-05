b0459561a5a95d3980470bc6b52545778f2072e9
Javohir Jumanov <jumanovjavohir49@gmail.com>
2022-12-05 22:58:16 +0500

    updates 10:57 PM

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/MaxProfit/MaxProfits.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.Arrays.MaxProfit;
+public class MaxProfits
+{
+    public static int MaxProfit(int[] prices)
+    {
+        int minPrice = prices[0], maxPrice = int.MinValue;
+
+        for (int iterator = 0; iterator < prices.Length; iterator++)
+        {
+            if (prices[iterator] < minPrice)
+                minPrice = prices[iterator];
+
+            maxPrice = Math.Max(maxPrice, prices[iterator] - minPrice);
+        }
+
+        if (maxPrice > 0)
+            return maxPrice;
+
+        return 0;
+    }
+}
---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/MaxSubArray/MaxSubArrays.cs
@@ -0,0 +1,45 @@
+﻿namespace DailyLeetcode.Arrays.MaxSubArray;
+
+public class MaxSubArrays
+{
+    public static int MaxSubArray(int[] nums)
+    {
+        // return OneSolution(nums);
+     
+        return TwoSolution(nums);
+    }
+    private static int OneSolution(int[] nums)
+    {
+        if (nums.Length == 1)
+            return nums[0];
+
+        int localMax = 0, globalMax = int.MinValue;
+
+        for (int iterator = 0; iterator < nums.Length; iterator++)
+        {
+            localMax = Math.Max(nums[iterator], nums[iterator] + localMax);
+            globalMax = Math.Max(localMax, globalMax);
+        }
+
+        return globalMax;
+    }
+    private static int TwoSolution(int[] nums)
+    {
+        int left = 0, right = 0, max = int.MinValue, SumLeft = 0;
+
+        while (right < nums.Length && left < nums.Length)
+        {
+            SumLeft += nums[right++];
+            max = Math.Max(max, SumLeft);
+
+            if (right == nums.Length)
+            {
+                left++;
+                right = left;
+                SumLeft = 0;
+            }
+        }
+
+        return max;
+    }
+}
