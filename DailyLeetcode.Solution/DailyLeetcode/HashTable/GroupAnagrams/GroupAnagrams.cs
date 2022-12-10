37a1d67565a3d76c131c31cbf19ec5006692cd90
Javohir Jumanov <jumanovjavohir49@gmail.com>
2022-12-10 17:00:38 +0500

    updates 5:00 PM

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/GroupAnagrams/GroupAnagrams.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.HashTable.GroupAnagrams;
+public class GroupAnagrams
+{
+    public static IList<IList<string>> GroupAnagramss(string[] strs)
+    {
+        Dictionary<string, IList<string>> dic = new Dictionary<string, IList<string>>();
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            string temp = String.Concat(strs[i].OrderBy(x => x));
+
+            if (!dic.ContainsKey(temp))
+                dic.Add(temp, new List<string>() { strs[i] });
+            else
+                dic[temp].Add(strs[i]);
+        }
+
+        return
+            dic.Values.ToList();
+    }
+}
