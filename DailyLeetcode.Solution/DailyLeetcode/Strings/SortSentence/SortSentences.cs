54b27df6ca128513cdbac1d7e96f12ffac120e24
Javohir Jumanov <jumanovjavohir49@gmail.com>
2022-12-14 02:15:42 +0500

    updates 2:15 PM

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/SortSentence/SortSentences.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Strings.SortSentence;
+public class SortSentences
+{
+    public static string SortSentence(string s)
+    {
+        var sortedDict = new SortedDictionary<int, string>();
+
+        string[] words = s.Split(' ');
+
+        for (int iterator = 0; iterator < words.Length; iterator++)
+        {
+            string word = words[iterator];
+            sortedDict.Add(word[word.Length - 1] - '0', word.Substring(0, word.Length - 1));
+        }
+
+        return
+            String.Join(" ", sortedDict.Values.ToList());
+    }
+}
