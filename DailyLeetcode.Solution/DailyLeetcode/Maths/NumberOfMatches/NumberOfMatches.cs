40d149c7ab6157ddb5af234cd5df97389464cc63
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-04 12:32:22 +0500

    Update 1/4/2023 12:32 AM

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/NumberOfMatches/NumberOfMatches.cs
@@ -0,0 +1,26 @@
+﻿namespace DailyLeetcode.Maths.NumberOfMatches;
+public class NumberOfMatches
+{
+    private static int NumberOfMatchess(int number)
+    {
+        int counter = 0;
+
+        while (number > 1)
+        {
+            if (number % 2 == 0)
+            {
+                counter += number / 2;
+
+                number -= number / 2;
+            }
+            else if (number % 2 == 1)
+            {
+                counter += (number - 1) / 2;
+
+                number -= (number - 1) / 2;
+            }
+        }
+
+        return counter;
+    }
+}
