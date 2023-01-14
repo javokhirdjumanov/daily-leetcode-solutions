9f7ec3896ce4b6b0e1d2b72b56a88f00c609438f
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-15 03:58:12 +0500

    added at Hasg table

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/FindDisappearedNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.HashTable;
+public sealed class FindDisappearedNumbers
+{
+    public static IList<int> FindDisappearedNumberss(int[] nums)
+    {
+        var result = new List<int>();
+
+        var set = new HashSet<int>(nums);
+
+        int param = 1;
+        while (param <= nums.Length)
+        {
+            if (!set.Contains(param))
+                result.Add(param);
+
+            param++;
+        }
+
+        return result;
+    }
+}
