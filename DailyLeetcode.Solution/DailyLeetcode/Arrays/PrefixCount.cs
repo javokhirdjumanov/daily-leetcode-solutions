b02e986e8c022d546b67e58f9cb0ad79f9d97510
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-15 16:43:32 +0500

    added new solution to array

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/PrefixCount.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Arrays;
+public sealed class PrefixCount
+{
+    public static int PrefixCounts(string[] words, string pref)
+    {
+        int count = 0;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length >= pref.Length)
+            {
+                string s = words[i].Substring(0, pref.Length);
+                if (s == pref)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+}
