3fc7aa7bf69cd4a19076548e44ee2eef5bf9ace0
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-16 14:57:37 +0500

    Added new solution to Hash Table

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/AreOccurencisEqual.cs
@@ -0,0 +1,28 @@
+﻿namespace DailyLeetcode.HashTable;
+public sealed class AreOccurencisEqual
+{
+    public static bool AreOccurrencesEquals(string s)
+    {
+        var dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dict.ContainsKey(s[i].ToString()))
+            {
+                dict.Add(s[i].ToString(), 1);
+            }
+            else
+                dict[s[i].ToString()]++;
+        }
+
+        int temp = dict[s[0].ToString()];
+
+        foreach (var item in dict)
+        {
+            if (item.Value != temp)
+                return false;
+        }
+
+        return true;
+    }
+}
