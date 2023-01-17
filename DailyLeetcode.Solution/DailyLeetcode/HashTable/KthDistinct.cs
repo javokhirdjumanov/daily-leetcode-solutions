c324801b2b78f0ee98bf09593e9da3bb88bfedf0
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-17 10:22:30 +0500

    added two exercises to HashTable :)

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/KthDistinct.cs
@@ -0,0 +1,28 @@
+﻿namespace DailyLeetcode.HashTable;
+public sealed class KthDistinct
+{
+    public static string KthDistincts(string[] arr, int k)
+    {
+        var list = new List<string>();
+        var dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!dict.ContainsKey(arr[i].ToString()))
+                dict.Add(arr[i].ToString(), 1);
+            else
+                dict[arr[i].ToString()]++;
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value == 1)
+                list.Add(item.Key);
+        }
+
+        if (list.Count >= k)
+            return list[k - 1];
+
+        return "";
+    }
+}
