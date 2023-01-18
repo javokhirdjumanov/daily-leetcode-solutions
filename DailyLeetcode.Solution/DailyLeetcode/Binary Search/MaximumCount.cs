de8713bdfd69571fdcf6e4491d6aabb4ab8d0080
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-18 13:50:57 +0500

    Added new file Binary Search and added two exercises to Binary Search

---
+++ DailyLeetcode.Solution/DailyLeetcode/Binary Search/MaximumCount.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Binary_Search;
+public sealed class MaximumCount
+{
+    public static int MaximumCounts(int[] nums)
+    {
+        int pos = 0, neg = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] < 0)
+                neg++;
+            else if (nums[i] > 0)
+                pos++;
+        }
+        int max = Math.Max(pos, neg);
+
+        return max;
+    }
+}
