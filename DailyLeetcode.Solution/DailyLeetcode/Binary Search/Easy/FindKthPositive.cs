96082928d0e3bc9bdd7dbde4e50e1f8b2d265525
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-20 17:13:24 +0500

    Exercises added to Binary Search

---
+++ DailyLeetcode.Solution/DailyLeetcode/Binary Search/Easy/FindKthPositive.cs
@@ -0,0 +1,26 @@
+﻿namespace DailyLeetcode.Binary_Search.Easy;
+public sealed class FindKthPositive
+{
+    public int FindKthPositives(int[] arr, int k)
+    {
+        var list = new List<int>();
+
+        var set = FillTheHashSet(arr);
+
+        for (int i = 0; i < 1000 + arr.Length; i++)
+        {
+            if (!set.Contains(i + 1))
+                list.Add(i + 1);
+        }
+        return list[k - 1];
+    }
+    private HashSet<int> FillTheHashSet(int[] arr)
+    {
+        var set = new HashSet<int>();
+
+        for (int i = 0; i < arr.Length; i++)
+            set.Add(arr[i]);
+
+        return set;
+    }
+}
