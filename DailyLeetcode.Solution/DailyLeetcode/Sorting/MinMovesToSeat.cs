db7285771957697dd8ca4c64a94a2221d5c1729b
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-21 16:29:34 +0500

    some exercises added to Sorting folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/MinMovesToSeat.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.Sorting;
+public sealed class MinMovesToSeat
+{
+    private int MinMovesToSeats(int[] seats, int[] students)
+    {
+        Array.Sort(seats);
+        Array.Sort(students);
+
+        int counter = 0;
+
+        for (int i = 0; i < seats.Length; i++)
+        {
+            int temp = Math.Abs(seats[i] - students[i]);
+
+            counter += temp;
+        }
+
+        return counter;
+    }
+}
