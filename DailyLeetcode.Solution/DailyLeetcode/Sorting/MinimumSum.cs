aa52a05f537dbdb7a0bb933b43e9cf776ca82ce5
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-22 01:26:51 +0500

    added new exercises to Sorting

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/MinimumSum.cs
@@ -0,0 +1,15 @@
+﻿namespace DailyLeetcode.Sorting;
+public sealed class MinimumSum
+{
+    public int MinimumSums(int num)
+    {
+        int[] shit = Convert.ToString(num)
+            .Select(c => c - '0').ToArray();
+
+        Array.Sort(shit);
+
+        return
+            (shit[0] * 10 + shit[2]) + (shit[1] * 10 + shit[3]);
+
+    }
+}
