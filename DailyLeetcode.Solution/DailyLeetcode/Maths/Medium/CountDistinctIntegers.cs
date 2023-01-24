9b6b0aa5ad51d0e0f30f8240ca9204465111d754
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-25 00:46:31 +0500

    added CountDistinctInteger to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Medium/CountDistinctIntegers.cs
@@ -0,0 +1,32 @@
+﻿namespace DailyLeetcode.Maths.Medium;
+public sealed class CountDistinctIntegers
+{
+    private static int CountDistinctIntegerss(int[] nums)
+    {
+        var list = new List<int>(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int temp = ReverseInteger(nums[i]);
+
+            list.Add(temp);
+        }
+
+        var set = new HashSet<int>(list);
+
+        return set.Count;
+    }
+    private static int ReverseInteger(int number)
+    {
+        int summa = 0;
+
+        while (number > 0)
+        {
+            summa = summa * 10 + number % 10;
+
+            number /= 10;
+        }
+
+        return summa;
+    }
+}
