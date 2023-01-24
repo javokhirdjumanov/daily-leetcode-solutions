0f139dfc42855fe2b54cdc4efb6b551c485efc7b
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-25 00:43:09 +0500

    added convert_temperature to Math folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easy/ConvertTemperatures.cs
@@ -0,0 +1,13 @@
+﻿namespace DailyLeetcode.Maths.Easy;
+public sealed class ConvertTemperatures
+{
+    public static double[] ConvertTemperaturess(double celsius)
+    {
+        double[] result = new double[2];
+
+        result[0] = celsius + 273.15;
+        result[1] = celsius * 1.80 + 32.00;
+
+        return result;
+    }
+}
