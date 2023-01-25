73d92c1219da17edb56eb431e13ea30e600afc58
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-26 00:47:16 +0500

    Easy changes Easies for Array folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/ContainsNearbyDuplicates.cs
@@ -0,0 +1,27 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+public class ContainsNearbyDuplicates
+{
+    public static bool ContainsNearbyDuplicate(int[] nums, int j_iteirator)
+    {
+        int left = 0, right = left + 1;
+
+        while (left < nums.Length && right < nums.Length)
+        {
+            if (nums[left] == nums[right])
+            {
+                if (Math.Abs(left - right) <= j_iteirator)
+                    return true;
+            }
+
+            right++;
+
+            if (right == nums.Length)
+            {
+                left++;
+                right = left + 1;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/SortPeople.cs
@@ -0,0 +1,24 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+public class SortPeople
+{
+    public string[] SortPeoples(string[] names, int[] heights)
+    {
+        var sortedDict = new SortedDictionary<int, string>();
+
+        int heightsIndex = 0, namesIndex = 0;
+
+        while (namesIndex < names.Length)
+        {
+            sortedDict.Add(heights[heightsIndex++], names[namesIndex++]);
+        }
+
+        var result = new List<string>();
+
+        foreach (string val in sortedDict.Values)
+            result.Add(val);
+
+        result.Reverse();
+
+        return result.ToArray();
+    }
+}
