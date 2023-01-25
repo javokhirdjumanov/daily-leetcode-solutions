74e2829817d0b7bab6fc9a604e890857461d780e
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-26 00:35:41 +0500

    fixed maybe wrong aswers for Binary Search

---
+++ DailyLeetcode.Solution/DailyLeetcode/Binary Search/Easies/TargetIndices.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.Binary_Search;
+public sealed class TargetIndices
+{
+    public static IList<int> TargetIndicess(int[] nums, int target)
+    {
+        var result = new List<int>();
+
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
