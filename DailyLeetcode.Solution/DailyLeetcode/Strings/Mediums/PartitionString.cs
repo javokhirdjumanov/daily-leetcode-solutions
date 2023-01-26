8caa86c6d52ecce95b1372a70b7c56d1a8610834
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-27 00:53:31 +0500

    new exercises String  Medium folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Mediums/PartitionString.cs
@@ -0,0 +1,28 @@
+﻿namespace DailyLeetcode.Strings.Mediums;
+public sealed class PartitionString
+{
+    private static int PartitionStrings(string s)
+    {
+        var list = new List<string>();
+        var set = new HashSet<string>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (set.Contains(s[i].ToString()))
+            {
+                list.Add(String.Join("", set.ToArray()));
+
+                set = new HashSet<string>();
+            }
+
+            set.Add(s[i].ToString());
+
+            if (i == s.Length - 1)
+            {
+                list.Add(String.Join("", set.ToArray()));
+            }
+        }
+
+        return list.Count;
+    }
+}
