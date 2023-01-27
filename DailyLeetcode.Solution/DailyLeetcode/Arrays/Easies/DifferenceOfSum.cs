3958df49d00af397a380abb8f3660e82d0e22dd2
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-27 23:45:17 +0500

    Added new solution to Easy of Array folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/DifferenceOfSum.cs
@@ -0,0 +1,29 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+public sealed class DifferenceOfSum
+{
+    public static int DifferenceOfSumma(int[] nums)
+    {
+        int summa = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+            summa += nums[i];
+
+        for (int i = 0; i < nums.Length; i++)
+            summa -= FragmentationNumber(number: nums[i]);
+
+        return summa;
+    }
+    private static int FragmentationNumber(int number)
+    {
+        int summa = 0;
+
+        while (number > 0)
+        {
+            summa += number % 10;
+
+            number /= 10;
+        }
+
+        return summa;
+    }
+}
