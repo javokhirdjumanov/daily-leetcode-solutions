f596d73d7c75d0777bbe50e5905cff8e9ef9a698
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-27 23:45:58 +0500

    Added new solution to medium of Array folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Mediums/SortTheStudents.cs
@@ -0,0 +1,32 @@
+﻿namespace DailyLeetcode.Arrays.Mediums;
+public sealed class SortTheStudents
+{
+    public static int[][] SortTheStudentss(int[][] score, int k)
+    {
+        var result = new int[score.Length][];
+
+        var sortedArray = SortedElementInScore(score: score, k: k);
+        for (int i = 0; i < sortedArray.Length; i++)
+        {
+            for (int j = 0; j < score.Length; j++)
+            {
+                if (sortedArray[i] == score[j][k])
+                {
+                    result[i] = score[j];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    private static int[] SortedElementInScore(int[][] score, int k)
+    {
+        var sortedArray = new int[score.Length];
+
+        for (int i = 0; i < score.Length; i++)
+            sortedArray[i] = score[i][k];
+
+        return
+            sortedArray.OrderByDescending(c => c).ToArray();
+    }
+}
