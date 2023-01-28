322a76d861dc23d7ead9812ff6a17d6c4d073bbb
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-28 20:35:48 +0500

    Added new solution in mediumof Sorted folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/Mediums/MinPairSum.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.Sorting.Mediums;
+public sealed class MinPairSum
+{
+    public static int MinPairSumma(int[] nums)
+    {
+        int maximal = 0;
+        int left = 0, right = nums.Length - 1;
+
+        Array.Sort(nums);
+
+        while (left < right)
+        {
+            int temp =
+                nums[right--] + nums[left++];
+
+            maximal = Math.Max(maximal, temp);
+        }
+
+        return maximal;
+    }
+}
