dcb2543bda49c34c75500a6ee04845e10b17be0b
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-28 20:35:22 +0500

    Added new solution in easy of Sorted folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/Easies/FindFinalValue.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyLeetcode.Sorting.Easies;
+public sealed class FindFinalValue
+{
+    public static int FindFinalValues(int[] nums, int original)
+    {
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == original)
+                original = original * 2;
+        }
+
+        return original;
+    }
+}
