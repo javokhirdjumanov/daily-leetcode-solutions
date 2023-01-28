d5f6b62b37a533ab07e5fc60755fededbaf98316
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-28 20:35:35 +0500

    Added new solution in medium of Sorted folder

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/Mediums/MaxCoins.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Sorting.Mediums;
+public sealed class MaxCoins
+{
+    public static int MaxCoinss(int[] piles)
+    {
+        int myAllCount = 0;
+        int left = 0, right = piles.Length - 2;
+
+        Array.Sort(piles);
+        while (left < right)
+        {
+            myAllCount += piles[right];
+            left++;
+            right -= 2;
+        }
+
+        return myAllCount;
+    }
+}
