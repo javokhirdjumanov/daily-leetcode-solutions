e2150f18c77827892063d79861ad17d42473da13
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-29 17:06:37 +0500

    Added new solution easy of Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/CountDigits.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class CountDigits
+{
+    public static int CountDigitss(int num)
+    {
+        int temp = num, counter = 0;
+
+        while (temp > 0)
+        {
+            if (num % (temp % 10) == 0)
+                counter++;
+
+            temp /= 10;
+        }
+
+        return counter;
+    }
+}
+
