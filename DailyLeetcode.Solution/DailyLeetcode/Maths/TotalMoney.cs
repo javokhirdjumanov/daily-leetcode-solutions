81cb92da856a7e9ab62a1c9151c6138c60aab5f7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-29 18:04:20 +0500

    Added new solution easy of Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/TotalMoney.cs
@@ -0,0 +1,26 @@
+﻿namespace DailyLeetcode.Maths;
+public sealed class TotalMoney
+{
+    public static int TotalMoneies(int n)
+    {
+        int summa = 0;
+        int dollarSumma = 1, dayOfWeek = 1, level = 0;
+
+        while (n > 0)
+        {
+            summa += dayOfWeek;
+
+            dayOfWeek++;
+            if (dayOfWeek == 8 + level)
+            {
+                dollarSumma += 1;
+                dayOfWeek = dollarSumma;
+
+                level++;
+            }
+            n--;
+        }
+
+        return summa;
+    }
+}
