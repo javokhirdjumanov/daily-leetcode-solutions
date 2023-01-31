2079606f20da982e850cfb0ea0093a1c9f32de52
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-31 20:29:59 +0500

    added new solutions medium to Array

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Mediums/MaxArea.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyLeetcode.Arrays.Mediums;
+public sealed class MaxArea
+{
+    public int MaxAreas(int[] height)
+    {
+        int max = 0, left = 0, right = height.Length - 1;
+
+        while (right > 0)
+        {
+            int temp = Math.Min(height[left], height[right]);
+
+            max = Math.Max(((right - left) * temp), max);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return max;
+    }
+}
