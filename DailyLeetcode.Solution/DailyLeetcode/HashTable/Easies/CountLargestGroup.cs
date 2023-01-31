ec5788475d71799cf74e9bec682b064b66ef1f3a
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-31 20:30:47 +0500

    added new solution Hash Table

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/Easies/CountLargestGroup.cs
@@ -0,0 +1,41 @@
+﻿namespace DailyLeetcode.HashTable.Easies;
+public sealed class CountLargestGroup
+{
+    public static int CountLargestGroups(int n)
+    {
+        var dict = new Dictionary<int, IList<int>>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            int temp = i, summa = 0;
+
+            while (temp > 0)
+            {
+                summa += temp % 10;
+
+                temp /= 10;
+            }
+
+            if (!dict.ContainsKey(summa))
+                dict.Add(summa, new List<int>() { i });
+            else
+                dict[summa].Add(i);
+        }
+
+        return MaxLengthFromDictValues(dict);
+    }
+    public static int MaxLengthFromDictValues(Dictionary<int, IList<int>> dict)
+    {
+        var dictionary = new SortedDictionary<int, int>();
+
+        foreach (var item in dict)
+        {
+            if (!dictionary.ContainsKey(item.Value.Count))
+                dictionary.Add(item.Value.Count, 1);
+            else
+                dictionary[item.Value.Count]++;
+        }
+
+        return dictionary.Values.Last();
+    }
+}
