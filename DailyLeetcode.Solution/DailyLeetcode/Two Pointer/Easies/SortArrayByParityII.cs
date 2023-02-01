9b469ed8190f6525ccd7c25039aed58a658f7b77
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-02 01:59:24 +0500

    added new solution to Two Pointer

---
+++ DailyLeetcode.Solution/DailyLeetcode/Two Pointer/Easies/SortArrayByParityII.cs
@@ -0,0 +1,26 @@
+﻿namespace DailyLeetcode.Two_Pointer.Easies;
+public sealed class SortArrayByParityII
+{
+    public static int[] SortArrayByParitiesII(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        int even = 0, odd = 1, index = 0;
+        while (index < nums.Length)
+        {
+            if (nums[index] % 2 == 0)
+            {
+                result[even] = nums[index];
+                even += 2;
+            }
+            else
+            {
+                result[odd] = nums[index];
+                odd += 2;
+            }
+            index++;
+        }
+
+        return result;
+    }
+}
