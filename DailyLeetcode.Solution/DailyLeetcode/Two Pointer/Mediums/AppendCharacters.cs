89a192a9f9137d8a2ddc32823e12f89f87b55bf0
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-02 01:59:53 +0500

    added new medium solution to Two Pointer

---
+++ DailyLeetcode.Solution/DailyLeetcode/Two Pointer/Mediums/AppendCharacters.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyLeetcode.Two_Pointer.Mediums;
+public sealed class AppendCharacters
+{
+    public static int AppendCharacterss(string s, string t)
+    {
+        int indexS = 0, indexT = 0, length = 0;
+
+        while (indexT < t.Length && indexS < s.Length)
+        {
+            if (s[indexS] == t[indexT])
+            {
+                length++;
+
+                indexS++; indexT++;
+            }
+            else
+                indexS++;
+        }
+
+        return t.Length - length;
+    }
+}
