33132e1e9f4762e157f032a7d28802a7b6021af8
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-01 18:27:17 +0500

    added new solution to String

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Easies/GcdOfStrings.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+namespace DailyLeetcode.Strings.Easies;
+public sealed class GcdOfStrings
+{
+    public static string GcdOfStringss(string str1, string str2)
+    {
+        string key = "";
+        for (int i = str2.Length; i > 0; i--)
+        {
+            key = str2.Substring(0, i);
+
+            if (str1.Length % key.Length == 0)
+            {
+                if (IsThereKeyInStr(str2, key))
+                {
+                    if (IsThereKeyInStr(str1, key))
+                        return key;
+                }
+            }
+            key = "";
+        }
+        return key;
+    }
+    private static bool IsThereKeyInStr(string s, string key)
+    {
+        var sb = new StringBuilder();
+        int length = s.Length / key.Length;
+
+        while (length > 0)
+        {
+            sb.Append(key);
+            length--;
+        }
+
+        if (sb.ToString() == s)
+            return true;
+
+        return false;
+    }
+}
