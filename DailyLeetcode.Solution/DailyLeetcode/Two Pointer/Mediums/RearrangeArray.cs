a9f2e7b613a8b2497d8a9c8e2ac6c3fa0b64870d
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-02 01:59:58 +0500

    added new medium solution to Two Pointer

---
+++ DailyLeetcode.Solution/DailyLeetcode/Two Pointer/Mediums/RearrangeArray.cs
@@ -0,0 +1,27 @@
+﻿namespace DailyLeetcode.Two_Pointer.Mediums;
+public sealed class RearrangeArray
+{
+    public static int[] RearrangeArrays(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        int index = 0, positive = 0, negative = 1;
+
+        while (index < nums.Length)
+        {
+            if (nums[index] > 0)
+            {
+                result[positive] = nums[index];
+                positive += 2;
+            }
+            else
+            {
+                result[negative] = nums[index];
+                negative += 2;
+            }
+            index++;
+        }
+
+        return result;
+    }
+}
