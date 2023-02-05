e310140deba86b990fe1698079340b4943499515
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-05 16:07:52 +0500

    added new solution to String

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Mediums/FindAnagrams.cs
@@ -0,0 +1,34 @@
+﻿namespace DailyLeetcode.Strings.Mediums;
+public sealed class FindAnagrams
+{
+    public static IList<int> FindAnagramss(string s, string p)
+    {
+        var result = new List<int>();
+        for (int i = 0; i < s.Length - p.Length + 1; i++)
+        {
+            string cut = s.Substring(i, p.Length);
+            if (IsAnagramm(cut, p))
+                result.Add(i);
+        }
+
+        return result;
+    }
+    private static bool IsAnagramm(string cut, string p)
+    {
+        int[] chars = new int[26];
+
+        for (int i = 0; i < cut.Length; i++)
+            chars[cut[i] - 97]++;
+
+        for (int i = 0; i < p.Length; i++)
+            chars[p[i] - 97]--;
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
