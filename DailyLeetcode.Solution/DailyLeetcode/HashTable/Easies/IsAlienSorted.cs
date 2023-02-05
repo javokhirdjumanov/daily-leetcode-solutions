629d5479b7b821edc84d221744e2dfbbd71e61d6
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-05 16:07:36 +0500

    added new solution to HashTable

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/Easies/IsAlienSorted.cs
@@ -0,0 +1,47 @@
+﻿namespace DailyLeetcode.HashTable.Easies;
+public sealed class IsAlienSorted
+{
+    public static Dictionary<string, int> AlphabetOrder(string order)
+    {
+        var dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < order.Length; i++)
+            dict.Add(order[i].ToString(), i + 1);
+
+        return dict;
+    }
+    public static bool IsAlienSorteds(string[] words, string order)
+    {
+        var orderalphabet = AlphabetOrder(order);
+
+        bool result = true, check = false;
+        string temp1 = "", temp2 = "";
+
+        for (int i = 1; i < words.Length; i++)
+        {
+            temp1 = words[i - 1]; temp2 = words[i];
+            int min = Math.Min(temp1.Length, temp2.Length);
+
+            for (int j = 0; j < min; j++)
+            {
+                if (orderalphabet[temp1[j].ToString()] < orderalphabet[temp2[j].ToString()])
+                {
+                    result = true;
+                    check = false;
+                    break;
+                }
+                else if (orderalphabet[temp1[j].ToString()] == orderalphabet[temp2[j].ToString()])
+                {
+                    check = true;
+                    continue;
+                }
+                else
+                    return false;
+            }
+        }
+        if (check && temp1.Length > temp2.Length)
+            return false;
+
+        return result;
+    }
+}
