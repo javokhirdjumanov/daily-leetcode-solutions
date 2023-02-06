64ecb817f5d7b967a214afdb3dd970176c58b334
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-06 14:44:41 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Easies/IsPowerOfFour.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Easies;
+public sealed class IsPowerOfFour
+{
+    public static bool IsPowerOfFours(int n)
+    {
+        if (n == 0 || n < 0) return false;
+        int check = 0;
+
+        while (n > 1)
+        {
+            check += n % 4;
+
+            n /= 4;
+        }
+        if (check == 0)
+            return true;
+
+        return false;
+    }
+}
