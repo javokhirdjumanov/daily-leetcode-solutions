511e6a6e5382f1c4c9a15711635ac8cc0219c767
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-06 14:44:47 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Easies/IsPowerOfThree.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Easies;
+public sealed class IsPowerOfThree
+{
+    public static bool IsPowerOfThrees(int n)
+    {
+        if (n == 0 || n < 0) return false;
+
+        int check = 0;
+
+        while (n > 1)
+        {
+            check += n % 3;
+
+            n /= 3;
+        }
+        if (check == 0)
+            return true;
+
+        return false;
+    }
+}
