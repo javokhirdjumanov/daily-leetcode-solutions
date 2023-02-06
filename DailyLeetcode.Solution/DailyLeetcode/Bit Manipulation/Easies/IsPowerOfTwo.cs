a8ffb0722340c7b4c6d8ac929dc9f52686c7a8cc
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-06 14:44:52 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Easies/IsPowerOfTwo.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Easies;
+public sealed class IsPowerOfTwo
+{
+    public static bool IsPowerOfTwos(int n)
+    {
+        if (n == 0 || n < 0) return false;
+
+        int check = 0;
+
+        while (n > 1)
+        {
+            check += n % 2;
+
+            n /= 2;
+        }
+        if (check == 0)
+            return true;
+
+        return false;
+    }
+}
