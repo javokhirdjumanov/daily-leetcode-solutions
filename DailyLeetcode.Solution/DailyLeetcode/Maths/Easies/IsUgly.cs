db50f41dcf7087c87c4b4437e0323c09c05bade6
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-09 18:06:56 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/IsUgly.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class IsUgly
+{
+    public static bool IsUglies(int n)
+    {
+        if (n <= 0) return false;
+
+        foreach (int item in new int[] { 2, 3, 5 })
+            n = IsPrime(n, item);
+
+        return n == 1;
+    }
+    public static int IsPrime(int n, int item)
+    {
+        while (n % item == 0)
+            n /= item;
+
+        return n;
+    }
+}
