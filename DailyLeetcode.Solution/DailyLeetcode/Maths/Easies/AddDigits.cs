7e6c2293f209e545f4d1ec8f39471dbc3389764a
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-09 18:06:51 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/AddDigits.cs
@@ -0,0 +1,18 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class AddDigits
+{
+    public static int AddDigitss(int num)
+    {
+        int summa = 0;
+        while (num > 0)
+        {
+            summa += num % 10;
+            num /= 10;
+        }
+
+        if (summa > 9)
+            return AddDigitss(summa);
+
+        return summa;
+    }
+}
