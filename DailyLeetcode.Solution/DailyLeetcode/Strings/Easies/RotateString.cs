17bb63386c4eee7613ecca84561b5e27b0f87456
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-09 23:52:19 +0500

    Added new solution to String

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Easies/RotateString.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+namespace DailyLeetcode.Strings.Easies;
+public sealed class RotateString
+{
+    public static bool RotateStrings(string s, string goal)
+    {
+        var sb = new StringBuilder(s);
+
+        int i = 0;
+        while (i < s.Length)
+        {
+            sb.Append(s[i++]); sb.Remove(0, 1);
+
+            if (sb.ToString() == goal)
+                return true;
+        }
+
+        return false;
+    }
+}
