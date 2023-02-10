a325425d0440240ba2e7598e891293dd97d36b24
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-10 12:10:02 +0500

    Added new medium solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Mediums/Reverse.cs
@@ -0,0 +1,36 @@
+﻿namespace DailyLeetcode.Maths.Mediums;
+public sealed class Reverse
+{
+    public static int ReverseInteger(int x)
+    {
+        // checking the number for negativity
+        bool manfiy = false;
+        if (x < 0)
+        {
+            if (x == int.MinValue)
+                x = x + 1;
+
+            x = Math.Abs(x);
+            manfiy = true;
+        }
+
+        // reverse integer
+        long summa = 0;
+        while (x > 0)
+        {
+            summa = summa * 10 + x % 10;
+
+            if (summa > int.MaxValue)
+                return 0;
+
+            x /= 10;
+        }
+
+        // if number is negative
+        if (manfiy)
+            return ((int)summa * -1);
+
+        // else
+        return (int)summa;
+    }
+}
