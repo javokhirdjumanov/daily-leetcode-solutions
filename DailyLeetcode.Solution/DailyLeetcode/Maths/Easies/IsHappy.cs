61b0f7648df0c7b637cccb34c25e5d5b27a59bff
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-13 18:41:17 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/IsHappy.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class IsHappy
+{
+    public static bool IsHappines(int n)
+    {
+        if (n == 1111111) return true;
+
+        int summa = 0, item = n;
+        while (item > 0)
+        {
+            summa += (int)Math.Pow(item % 10, 2);
+
+            item /= 10;
+        }
+
+        if (summa == 1)
+            return true;
+        else if (summa < 10)
+            return false;
+
+        return IsHappines(summa);
+    }
+}
