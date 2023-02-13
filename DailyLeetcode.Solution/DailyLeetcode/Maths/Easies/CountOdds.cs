2baacb03ddf210c98a3aea879613ccdfa2be9e81
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-13 18:41:13 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/CountOdds.cs
@@ -0,0 +1,7 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class CountOdds
+{
+    public int CountOddss(int low, int high)
+        => (high % 2 == 0 && low % 2 == 0) ? (high - low) / 2 : (high - low) / 2 + 1;
+
+}
