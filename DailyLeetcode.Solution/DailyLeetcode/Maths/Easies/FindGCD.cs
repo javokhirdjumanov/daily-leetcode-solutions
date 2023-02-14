dbfcd37b5bda6612d24e0bd88c10ab17bf02e943
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-15 04:08:24 +0500

    add new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/FindGCD.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public class FindGCD
+{
+    public static int FindGCDs(int[] nums)
+    {
+        var maximalElement = nums.Max();
+        var minimalElement = nums.Min();
+
+        int cycle = minimalElement;
+        while (cycle > 0)
+        {
+            if (maximalElement % cycle == 0 && minimalElement % cycle == 0)
+                return cycle;
+
+            cycle--;
+        }
+
+        return 1;
+    }
+}
