00ba8e697cdcf4e5d03a926412ef89b2a0359a45
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-15 03:54:12 +0500

    added new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/IsSameAfterReversals.cs
@@ -0,0 +1,25 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class IsSameAfterReversals
+{
+    public static bool IsSameAfterReversalss(int num)
+    {
+        int reverb = 0, temp = num;
+
+        temp = ReverbNumber(temp, reverb);
+
+        reverb = 0;
+
+        return ReverbNumber(temp, reverb) == num;
+    }
+    private static int ReverbNumber(int temp, int reverb)
+    {
+        while (temp > 0)
+        {
+            reverb = reverb * 10 + temp % 10;
+
+            temp /= 10;
+        }
+
+        return reverb;
+    }
+}
