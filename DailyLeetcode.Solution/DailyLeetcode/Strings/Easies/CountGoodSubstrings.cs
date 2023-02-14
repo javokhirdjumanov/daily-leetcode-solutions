328a66790849adaeac0b733554477a40cb499b7d
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-15 02:08:50 +0500

    added new solution to string

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Easies/CountGoodSubstrings.cs
@@ -0,0 +1,18 @@
+﻿namespace DailyLeetcode.Strings.Easies;
+public sealed class CountGoodSubstrings
+{
+    public static int CountGoodSubstringss(string s)
+    {
+        int count = 0;
+
+        for (int i = 0; i < s.Length - 2; i++)
+        {
+            string temp = s.Substring(i, 3);
+
+            if (!(temp[0] == temp[1] || temp[0] == temp[2] || temp[2] == temp[1]))
+                count++;
+        }
+
+        return count;
+    }
+}
