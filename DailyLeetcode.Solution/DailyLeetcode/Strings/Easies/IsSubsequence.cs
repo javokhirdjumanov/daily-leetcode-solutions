2db5103be3dccc8a416e198e39bb76642152eac1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-16 15:47:26 +0500

    add new solution for String

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Easies/IsSubsequence.cs
@@ -0,0 +1,25 @@
+﻿namespace DailyLeetcode.Strings.Easies;
+public class IsSubsequence
+{
+    public static bool IsSubsequences(string s, string t)
+    {
+        if (s == "") return true;
+        int indexS = 0, indexT = 0; ;
+
+        while (indexS < s.Length && indexT < t.Length)
+        {
+            if (t[indexT].ToString() == s[indexS].ToString())
+                indexS++;
+            else
+            {
+                t = t.Remove(indexT, 1);
+                indexT--;
+            }
+            indexT++;
+        }
+        if (t.Length > s.Length)
+            t = t.Remove(s.Length);
+
+        return s == t;
+    }
+}
