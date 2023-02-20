8264f64d7fd18f7544323d0908371f9427a3674f
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-20 14:25:18 +0500

    added new solution to Bit Manipulation

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Easies/XorOperation.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Easies;
+internal class XorOperation
+{
+    public static int XorOperations(int n, int start)
+    {
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = start + 2 * i;
+            result ^= temp;
+        }
+
+        return result;
+    }
+}
