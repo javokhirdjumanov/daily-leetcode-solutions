09cb9beab8599e32337ca8285645eaebf0e9c3d6
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-20 14:24:51 +0500

    added new solution to Array

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/Intersection.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+public class Intersection
+{
+    public static int[] Intersections(int[] nums1, int[] nums2)
+    {
+        var set = new HashSet<int>(nums1);
+
+        foreach (var item in set)
+        {
+            if (!nums2.Contains<int>(item))
+                set.Remove(item);
+        }
+
+        return set.ToArray();
+    }
+}
