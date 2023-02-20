fc6e856cf8dcde7b0eaacdb7a3a435d616f8cc6c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-20 15:39:11 +0500

    add new solution to Array

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/IsMonotonic.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+internal class IsMonotonic
+{
+    public static bool IsMonotonics(int[] nums)
+    {
+        var increasing = nums.OrderBy(x => x).ToArray();
+        var decreasing = nums.OrderByDescending(x => x).ToArray();
+
+        if (Enumerable.SequenceEqual(increasing, nums) || Enumerable.SequenceEqual(decreasing, nums))
+            return true;
+
+        return false;
+    }
+}
