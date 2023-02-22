5c91be2edabebacfa58d4e45bdbb1733cafd1af3
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-22 19:58:20 +0500

    Add new solution to Matrix

---
+++ DailyLeetcode.Solution/DailyLeetcode/JuggedArray/Easies/TransposeMatrix.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.JuggedArray.Easies;
+internal class TransposeMatrix
+{
+    public int[][] Transpose(int[][] matrix)
+    {
+        int[][] result = new int[matrix[0].Length][];
+
+        for (int j = 0; j < matrix[0].Length; j++)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (result[j] == null)
+                    result[j] = new int[matrix.Length];
+
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+}
