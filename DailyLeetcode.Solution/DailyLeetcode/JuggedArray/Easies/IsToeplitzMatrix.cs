9549fb5f25cd5f8873351b95a3f69e56d8df6cc5
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-23 23:29:44 +0500

    add new solution to Jugged Array

---
+++ DailyLeetcode.Solution/DailyLeetcode/JuggedArray/Easies/IsToeplitzMatrix.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.JuggedArray.Easies;
+internal class IsToeplitzMatrix
+{
+    public static bool IsToeplitzMatrixs(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.Length - 1; i++)
+        {
+            int ii = i + 1;
+            for (int j = 0; j < matrix[0].Length - 1; j++)
+            {
+                int jj = j + 1;
+
+                if (matrix[i][j] != matrix[ii][jj])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
