05fe882741bf1ed5ca8450831bfb0e1d6bc7bdb1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-25 01:50:33 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Dynamic Programming/Easies/Fibonacci_Number.cs
@@ -0,0 +1,17 @@
+﻿namespace DailyLeetcode.Dynamic_Programming.Easies;
+internal class Fibonacci_Number
+{
+    public int Fib(int n)
+    {
+        if (n <= 1) return n;
+
+        int[] arr = new int[n + 1];
+        arr[1] == 1;
+        arr[2] == 1;
+
+        for (int i = 3; i <= n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+
+        return arr[n];
+    }
+}
