3cbdca638607ed87c1eb08523d99f177aa2ccd84
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-25 01:50:23 +0500

    add new files DP

---
+++ DailyLeetcode.Solution/DailyLeetcode/Dynamic Programming/Easies/ClimbStairs.cs
@@ -0,0 +1,17 @@
+﻿namespace DailyLeetcode.Dynamic_Programming.Easies;
+internal class ClimbStairs
+{
+    public int ClimbStairss(int n)
+    {
+        if (n == 1) return 1;
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+
+        return arr[n];
+    }
+}
