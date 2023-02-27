918f6dcf509caf3a686af9fb3b1fa46f2a49b773
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-27 21:26:16 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/Intersections.cs
@@ -0,0 +1,27 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+internal class Intersections
+{
+    public static IList<int> Intersection(int[][] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums[i].Length; j++)
+            {
+                if (!dict.ContainsKey(nums[i][j]))
+                    dict.Add(nums[i][j], 1);
+                else
+                    dict[nums[i][j]]++;
+            }
+        }
+
+        var result = new List<int>();
+        foreach (var item in dict)
+        {
+            if (item.Value == nums.Length)
+                result.Add(item.Key);
+        }
+
+        return result.OrderBy(x => x).ToList();
+    }
+}
