d08c06a4144257f2362ab40e11bb36bee75ce9f3
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-27 16:17:57 +0500

    add new solution to Sorting

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/Mediums/FindErrorNumss.cs
@@ -0,0 +1,31 @@
+﻿namespace DailyLeetcode.Sorting.Mediums
+{
+    internal class FindErrorNumss
+    {
+        public static int[] FindErrorNums(int[] nums)
+        {
+            var arr = ArrayWithUniqueElement(nums);
+            int duplicate = nums.GroupBy(x => x).SingleOrDefault(g => g.Count() > 1).Key;
+
+            List<int> result = new();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    result.Add(duplicate); result.Add(i + 1);
+                    break;
+                }
+            }
+            return result.ToArray();
+        }
+        public static int[] ArrayWithUniqueElement(int[] nums)
+        {
+            var arr = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+                arr[nums[i] - 1] = nums[i];
+
+            return arr;
+        }
+    }
+}
