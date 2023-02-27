fcf01129fa5375b41a86a7edae02905550f3e042
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-27 22:16:25 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/FindLuckys.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+internal class FindLuckys
+{
+    public static int FindLucky(int[] arr)
+    {
+        int maximalElement = 0;
+
+        Dictionary<int, int> map = new();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!map.ContainsKey(arr[i]))
+                map.Add(arr[i], 1);
+            else
+                map[arr[i]]++;
+        }
+        foreach (var item in map)
+        {
+            if (item.Value == item.Key)
+                maximalElement = Math.Max(maximalElement, item.Value);
+        }
+        return maximalElement;
+    }
+}
