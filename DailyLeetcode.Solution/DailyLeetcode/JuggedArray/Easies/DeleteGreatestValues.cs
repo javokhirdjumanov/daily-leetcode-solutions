7a1441353a3e37ef1dcc0272bd30d2eeda10ddc1
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-27 13:19:14 +0500

    add new solution to Jagged array

---
+++ DailyLeetcode.Solution/DailyLeetcode/JuggedArray/Easies/DeleteGreatestValues.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.JuggedArray.Easies;
+internal class DeleteGreatestValues
+{
+    public static int DeleteGreatestValue(int[][] grid)
+    {
+        grid = grid.Select(subArray => subArray
+                   .OrderBy(num => num).ToArray()).ToArray();
+
+        int maximal = 0, result = 0;
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                maximal = Math.Max(maximal, grid[i][j]);
+            }
+            result += maximal;
+        }
+
+        return result;
+    }
+}
