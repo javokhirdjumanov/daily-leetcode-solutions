01c209d9a29fda0430406df738d006eb5cd2a155
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-27 20:53:37 +0500

    add medium solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Sorting/Mediums/ArrangeWordss.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyLeetcode.Sorting.Mediums
+{
+    internal class ArrangeWordss
+    {
+        public static string ArrangeWords(string text)
+        {
+            var sortDict = new SortedDictionary<int, List<string>>();
+
+            var words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!sortDict.ContainsKey(words[i].Length))
+                    sortDict.Add(words[i].Length, new List<string>() { words[i].ToLower() });
+                else
+                    sortDict[words[i].Length].Add(words[i].ToLower());
+            }
+
+            var result = string.Join(" ", sortDict.Values.SelectMany(list => list));
+
+            return result.Substring(0, 1).ToUpper() + result.Substring(1); ;
+        }
+    }
+}
