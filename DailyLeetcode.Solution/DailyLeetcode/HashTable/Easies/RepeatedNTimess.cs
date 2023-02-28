c6d844672902304a3df2286c6a82d1b98aed917b
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-02-28 16:26:45 +0500

    add new solution to HashTable

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/Easies/RepeatedNTimess.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyLeetcode.HashTable.Easies;
+internal class RepeatedNTimess
+{
+    public static int RepeatedNTimes(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!dict.ContainsKey(nums[i]))
+                dict.Add(nums[i], 1);
+            else
+                dict[nums[i]]++;
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value == nums.Length / 2)
+                return item.Key;
+        }
+
+        return 0;
+    }
+}
