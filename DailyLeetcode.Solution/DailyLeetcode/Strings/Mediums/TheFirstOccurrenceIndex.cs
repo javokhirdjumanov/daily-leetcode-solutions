8f2676da89f7361b594f74a7820a0d74a3ce9768
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-03 10:08:51 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Mediums/TheFirstOccurrenceIndex.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyLeetcode.Strings.Mediums;
+internal class TheFirstOccurrenceIndex
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        for (int i = 0; i <= haystack.Length - needle.Length; i++)
+        {
+            string temp = haystack.Substring(i, needle.Length);
+            if (temp == needle)
+                return i;
+        }
+        return -1;
+    }
+}
