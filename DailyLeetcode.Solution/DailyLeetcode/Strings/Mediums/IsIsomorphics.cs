b25b596330277fd2252ade2651efbae01c56e478
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-05 18:14:04 +0500

    add new medium exercises

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Mediums/IsIsomorphics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyLeetcode.Strings.Mediums;
+internal class IsIsomorphics
+{
+    public static bool IsIsomorphic(string s, string t)
+    {
+        var dict = new Dictionary<string, string>();
+
+        var set = new HashSet<string>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dict.ContainsKey(s[i].ToString()))
+            {
+                if (!set.Contains(t[i].ToString()))
+                    set.Add(t[i].ToString());
+                else return false;
+
+                dict.Add(s[i].ToString(), t[i].ToString());
+            }
+            else
+            {
+                if (t[i].ToString() != dict[s[i].ToString()]) 
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
