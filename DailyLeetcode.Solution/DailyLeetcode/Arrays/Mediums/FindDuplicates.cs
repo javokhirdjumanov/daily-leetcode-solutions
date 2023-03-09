04145a86565cd51403738cc5ee018f1e7fe9f59b
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-09 19:03:19 +0500

    add new medium solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Mediums/FindDuplicates.cs
@@ -0,0 +1,24 @@
+﻿namespace DailyLeetcode.Arrays.Mediums;
+internal class FindDuplicates
+{
+    public static int FindDuplicate(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (int i in nums)
+        {
+            if (!dict.ContainsKey(i))
+                dict.Add(i, 1);
+            else
+                dict[i]++;
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value > 1)
+                return item.Key;
+        }
+
+        return 0;
+    }
+}
