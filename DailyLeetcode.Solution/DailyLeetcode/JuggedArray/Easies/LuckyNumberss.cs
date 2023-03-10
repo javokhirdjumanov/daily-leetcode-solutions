abb5d3831c5e89944bf15965e533ce982293470d
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-10 13:02:53 +0500

    add new solution to Matrix

---
+++ DailyLeetcode.Solution/DailyLeetcode/JuggedArray/Easies/LuckyNumberss.cs
@@ -0,0 +1,36 @@
+﻿namespace DailyLeetcode.JuggedArray.Easies;
+internal class LuckyNumberss
+{
+    public static IList<int> LuckyNumbers(int[][] matrix)
+    {
+        var list = new List<int>();
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            bool isHas = false;
+            int minimal = matrix[i].Min();
+
+            int column = FindTheColumn(minimal, matrix, i);
+
+            for (int j = 0; j < matrix.Length; j++)
+            {
+                if (matrix[j][column] > minimal)
+                {
+                    isHas = true; break;
+                }
+            }
+            if (!isHas)
+                list.Add(minimal);
+        }
+        return list;
+    }
+    public static int FindTheColumn(int minimal, int[][] matrix, int i)
+    {
+        for (int j = 0; j < matrix[i].Length; j++)
+        {
+            if (minimal == matrix[i][j])
+                return j;
+        }
+        return 0;
+    }
+}
