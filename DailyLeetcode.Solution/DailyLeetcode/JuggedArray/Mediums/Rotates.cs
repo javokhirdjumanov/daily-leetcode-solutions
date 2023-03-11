3f2396d15eae670143e1d5d0079d18ed0471cb51
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-11 13:34:19 +0500

    add new medium exercise to Jugged Array

---
+++ DailyLeetcode.Solution/DailyLeetcode/JuggedArray/Mediums/Rotates.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyLeetcode.JuggedArray.Mediums;
+internal class Rotates
+{
+    public static void Rotate(int[][] matrix)
+    {
+        var result = new int[matrix.Length][];
+
+        for (int i = 0; i < matrix[0].Length; i++)
+        {
+            for (int j = 0, index = matrix.Length - 1; j < matrix.Length && index >= 0; j++, index--)
+            {
+                if (result[i] == null)
+                    result[i] = new int[matrix.Length];
+
+                result[i][index] = matrix[j][i];
+            }
+        }
+
+        for (int i = 0; i < matrix.Length; i++)
+            for (int j = 0; j < matrix[i].Length; j++)
+                matrix[i][j] = result[i][j];
+    }
+}
