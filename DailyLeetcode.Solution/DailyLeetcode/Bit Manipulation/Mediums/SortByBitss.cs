6d0e892ee455765cc3d225eddfa6a843b7db6446
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-12 14:58:12 +0500

    add new meduim solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Mediums/SortByBitss.cs
@@ -0,0 +1,25 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Mediums;
+internal class SortByBitss
+{
+    public static int[] SortByBits(int[] arr)
+    {
+        var dict = new SortedDictionary<int, List<int>>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            string @bytes = Convert.ToString(arr[i], 2);
+
+            int sumOfDigitsOfNumber = @bytes.ToString()
+                .Select(digit => int.Parse(digit.ToString()))
+                .ToArray().Sum();
+
+            if (!dict.ContainsKey(sumOfDigitsOfNumber))
+                dict[sumOfDigitsOfNumber] = new List<int>();
+
+            dict[sumOfDigitsOfNumber].Add(arr[i]);
+        }
+
+        return dict.Values
+            .SelectMany(x => x.OrderBy(c => c)).ToArray();
+    }
+}
