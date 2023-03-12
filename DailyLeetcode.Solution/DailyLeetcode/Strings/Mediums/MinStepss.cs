550a214d78026f17d2d02b77d0e5998ed9a332da
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-12 16:14:50 +0500

    add new medium solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Mediums/MinStepss.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Strings.Mediums;
+internal class MinStepss
+{
+    public static int MinSteps(string s, string t)
+    {
+        int[] chars = new int[26];
+
+        for (int i = 0; i < s.Length; i++)
+            chars[s[i] - 'a']++;
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (chars[t[i] - 'a'] > 0)
+                chars[t[i] - 'a']--;
+        }
+
+        return chars.AsEnumerable().Sum();
+    }
+}
