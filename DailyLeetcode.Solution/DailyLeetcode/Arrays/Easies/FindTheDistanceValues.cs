2ebf64ba5eff1e2d321f056dcc571b77a6a59aaa
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-13 23:51:25 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Arrays/Easies/FindTheDistanceValues.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyLeetcode.Arrays.Easies;
+internal class FindTheDistanceValues
+{
+    public static int FindTheDistanceValue(int[] arr1, int[] arr2, int d)
+    {
+        int counter = 0;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            counter++;
+            for (int j = 0; j < arr2.Length; j++)
+            {
+                if (Math.Abs(arr1[i] - arr2[j]) <= d)
+                {
+                    counter--; break;
+                }
+            }
+        }
+
+        return counter;
+    }
+}
