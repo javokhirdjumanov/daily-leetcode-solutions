cf6ee5924e26bf728a77142823bc98ec6b6524be
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-14 23:12:50 +0500

    add new solution to Tree

---
+++ DailyLeetcode.Solution/DailyLeetcode/Trees/Mediums/SumNumbers.cs
@@ -0,0 +1,41 @@
+﻿using DailyLeetcode.Trees.Easies;
+
+namespace DailyLeetcode.Trees.Mediums;
+public class SumNumbers
+{
+    private static int result = 0;
+    public static int SumNumber(TreeNode root)
+    {
+        SummaOfThreeNodeVal(root, 0);
+        return result;
+    }
+    public static void SummaOfThreeNodeVal(TreeNode root, int summa)
+    {
+        summa = summa * 10 + root.val;
+        if (root.left is not null)
+        {
+            SummaOfThreeNodeVal(root.left, summa);
+        }
+        if (root.right is not null)
+        {
+            SummaOfThreeNodeVal(root.right, summa);
+        }
+        if (root.right is null && root.left is null)
+        {
+            result += summa;
+            return;
+        }
+    }
+}
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
