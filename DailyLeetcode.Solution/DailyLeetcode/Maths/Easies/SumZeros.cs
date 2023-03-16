ca8c7313f666ccc76204e40fad20333e662d95a4
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-17 01:20:07 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/SumZeros.cs
@@ -0,0 +1,15 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public sealed class SumZeros
+{
+    private static int[] SumZero(int n)
+    {
+        var list = new List<int>();
+
+        for (int i = 1; i < n; i++)
+            list.Add(i);
+
+        list.Add(-list.Sum(x => x));
+
+        return list.ToArray();
+    }
+}
