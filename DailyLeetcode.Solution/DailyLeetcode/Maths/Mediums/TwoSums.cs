38b62bc6ab1523d91508071661a6ce15c382d992
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-17 23:50:41 +0500

    add new medium

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Mediums/TwoSums.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyLeetcode.Maths.Mediums;
+internal class TwoSums
+{
+    public static int[] TwoSum(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] + numbers[j] == target)
+                {
+                    numbers[0] = i + 1; numbers[1] = j + 1;
+                    Array.Resize(ref numbers, 2);
+                    return numbers;
+                }
+            }
+        }
+        return null;
+    }
+}
