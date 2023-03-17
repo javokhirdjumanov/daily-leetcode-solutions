16c6d0889af80a71a182d38a831a6d889a0a5920
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-18 01:31:38 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Easies/CountBitss.cs
@@ -0,0 +1,19 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Easies;
+internal class CountBitss
+{
+    public static int[] CountBits(int n)
+    {
+        var list = new List<int>();
+        for (int i = 0; i <= n; i++)
+        {
+            int sum = Convert
+                .ToString(i, 2)
+                .Select(c => int.Parse(c.ToString()))
+                .Sum();
+
+            list.Add(sum);
+        }
+
+        return list.ToArray();
+    }
+}
