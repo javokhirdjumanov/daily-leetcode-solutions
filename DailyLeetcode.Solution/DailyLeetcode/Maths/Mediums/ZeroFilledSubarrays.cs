c1486d8f7b91f5bc2de33712e7f44f04de1fd9b4
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-22 19:42:31 +0500

    add new solutions

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Mediums/ZeroFilledSubarrays.cs
@@ -0,0 +1,18 @@
+﻿namespace DailyLeetcode.Maths.Mediums;
+internal class ZeroFilledSubarrays
+{
+    public static long ZeroFilledSubarray(int[] nums)
+    {
+        long result = 0; int zCnt = 0;
+        foreach (int num in nums)
+        {
+            if (num == 0)
+                zCnt++;
+            else
+                zCnt = 0;
+
+            result += zCnt;
+        }
+        return result;
+    }
+}
