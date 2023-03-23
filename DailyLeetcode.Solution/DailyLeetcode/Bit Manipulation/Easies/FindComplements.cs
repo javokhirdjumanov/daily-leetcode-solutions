8c5c9730d75f092cd0244b6e4fac354f8b57047c
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-23 22:09:50 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Bit Manipulation/Easies/FindComplements.cs
@@ -0,0 +1,18 @@
+﻿namespace DailyLeetcode.Bit_Manipulation.Easies;
+internal class FindComplements
+{
+    public static int FindComplement(int num)
+    {
+        string replaceBits = new string(Convert
+                        .ToString(num, 2)
+                        .Select(c => c == '0' ? '1' : '0')
+                        .ToArray());
+
+        int length = replaceBits.Length - 1, result = 0;
+
+        for (int i = 0; i < replaceBits.Length; i++)
+            result += (int)((replaceBits[i] - 48) * Math.Pow(2, length--));
+
+        return result;
+    }
+}
