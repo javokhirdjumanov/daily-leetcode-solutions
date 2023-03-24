0e8de80a0014a179a718f70353788889c414fba3
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-24 16:11:56 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Easies/DetectCapitalUses.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DailyLeetcode.Strings.Easies;
+internal class DetectCapitalUses
+{
+    public static bool DetectCapitalUse(string word)
+    {
+        return
+            Regex.IsMatch(word, @"^[A-Z]+$") ||
+            Regex.IsMatch(word, @"^[a-z]+$") ||
+            Regex.IsMatch(word, @"^[A-Z]{1}[a-z]*$");
+    }
+}
