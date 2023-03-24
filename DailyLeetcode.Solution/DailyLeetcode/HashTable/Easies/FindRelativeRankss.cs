dca3dfd416efe0091add8ca2bcfad213f7587911
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-03-24 15:31:20 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/HashTable/Easies/FindRelativeRankss.cs
@@ -0,0 +1,30 @@
+﻿namespace DailyLeetcode.HashTable.Easies;
+internal class FindRelativeRankss
+{
+    public static string[] FindRelativeRanks(int[] score)
+    {
+        var result = new List<string>();
+
+        var map = Rank(score);
+        for (int i = 0; i < score.Length; i++)
+            result.Add(map[score[i]]);
+
+        return result.ToArray();
+    }
+    public static Dictionary<int, string> Rank(int[] score)
+    {
+        var map = new Dictionary<int, string>();
+        var mold = new List<string>() { "Gold Medal", "Silver Medal", "Bronze Medal", };
+
+        for (int i = 4; i <= score.Length; i++)
+            mold.Add($"{i}");
+
+        score = score.OrderByDescending(x => x).ToArray();
+
+        for (int i = 0; i < score.Length; i++)
+            map.Add(score[i], mold[i]);
+
+        return map;
+    }
+    // Link: https://leetcode.com/problems/relative-ranks/submissions/921236059/
+}
