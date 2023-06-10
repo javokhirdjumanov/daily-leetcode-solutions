7939b24ab95f880e3267bedcc3651e245588cf45
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-06-10 17:12:34 +0500

    add new solution

---
+++ DailyLeetcode.Solution/DailyLeetcode/Database/Easies/1179_Reformat_Department_Table.sql
@@ -0,0 +1,19 @@
+﻿SELECT id, 
+    JAN AS Jan_Revenue,
+    FEB AS Feb_Revenue, 
+    MAR AS Mar_Revenue, 
+    APR AS Apr_Revenue, 
+    MAY AS May_Revenue,
+    JUN AS Jun_Revenue,
+    JUL AS Jul_Revenue, 
+    AUG AS Aug_Revenue,
+    SEP AS Sep_Revenue, 
+    OCT AS Oct_Revenue,
+    NOV AS Nov_Revenue,
+    DEC AS Dec_Revenue
+FROM Department
+PIVOT
+(
+    SUM(revenue)
+    FOR month IN (JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC)
+)P
