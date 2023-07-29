9a1c3d03c03b2bdd9031ccbc298e5f14ed7a58f5
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-07-29 11:09:16 +0500

    add new solution to Math

---
+++ DailyLeetcode.Solution/DailyLeetcode/Maths/Easies/2769_FindTheMaximumAchievableNumber.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyLeetcode.Maths.Easies;
+public class _2769_FindTheMaximumAchievableNumber
+{
+    public int TheMaximumAchievableX(int num, int t)
+    {
+        return num + 2 * t;
+    }
+}
