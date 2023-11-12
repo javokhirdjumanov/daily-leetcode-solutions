e24b239690d917f1cb3af87c3a7630908d005139
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-11-12 13:07:17 +0500

    add

--- DailyLeetcode.Solution/DailyLeetcode/Database/Easies/1327_List_Products_Ordered_Period.sql
+++ DailyLeetcode.Solution/DailyLeetcode/Database/Easies/1327_List_Products_Ordered_Period.sql
@@ -3,4 +3,4 @@ FROM Products AS pro
 RIGHT JOIN Orders AS ord ON ord.product_id = pro.product_id
 WHERE ord.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY pro.product_name
-HAVING SUM(ord.unit) > 99; 
+HAVING SUM(ord.unit) > 99;
