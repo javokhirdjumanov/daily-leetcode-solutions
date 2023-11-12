63e733885a147e41dc584f78b68ae786dca625b9
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-11-12 13:28:01 +0500

    add

---
+++ DailyLeetcode.Solution/DailyLeetcode/Strings/Easies/2828._Check_String_Acronym_Words.cs
@@ -0,0 +1,17 @@
+﻿namespace DailyLeetcode.Strings.Easies
+{
+    public class _2828
+    {
+        public bool IsAcronym(IList<string> words, string s)
+        {
+            if (words.Count != s.Length)
+                return false;
+
+            for (int i = 0; i < words.Count; i++)
+                if (words[i][0] != s[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
