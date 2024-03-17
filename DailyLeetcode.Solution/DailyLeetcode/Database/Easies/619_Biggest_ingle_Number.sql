78125af5f23b2a649b0c9b00474b51c4c7693ce7
Javohir Jumanov <jumanovjavohir49@gmail.com>
2024-03-17 17:33:10 +0500

    changes

---
+++ DailyLeetcode.Solution/DailyLeetcode/Database/Easies/619_Biggest_ingle_Number.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
